dd0d13788e88a13d79ad0d428527fc94129c5803
machiela <machielamj@ncias-d1308-v.nci.nih.gov>
2014-10-30 16:58:36 -0400

    Add MAF calculations

--- LDlink/LDproxy_sub.py
+++ LDlink/LDproxy_sub.py
@@ -13,7 +13,7 @@ print "Process "+str(int(process)+1)+" initialized."
 pop_list=open("pops_"+request+".txt").readlines()
 ids=[]
 for i in range(len(pop_list)):
-    ids.append(pop_list[i].strip())
+	ids.append(pop_list[i].strip())
 
 pop_ids=list(set(ids))
 
@@ -34,57 +34,61 @@ subprocess.call(grep_remove_dups, shell=True)
 
 # Define function to calculate LD metrics
 def LD_calcs(hap,allele,allele_n):
-    # Extract haplotypes
-    A=hap["00"]
-    B=hap["01"]
-    C=hap["10"]
-    D=hap["11"]
-    
-    N=A+B+C+D
-    delta=float(A*D-B*C)
-    Ms=float((A+C)*(B+D)*(A+B)*(C+D))
-    
-    if Ms!=0:
-        
-        # D prime
-        if delta<0:
-            D_prime=delta/min((A+C)*(A+B),(B+D)*(C+D))
-        else:
-            D_prime=delta/min((A+C)*(C+D),(A+B)*(B+D))
-        
-        # R2
-        r2=(delta**2)/Ms
-        
-        # Find Correlated Alleles
-        if r2>0.1:
-            
-            # Expected Cell Counts
-            eA=(A+B)*(A+C)/N
-            eB=(B+A)*(B+D)/N
-            eC=(C+A)*(C+D)/N
-            eD=(D+C)*(D+B)/N
-            
-            # Calculate Deltas
-            dA=(A-eA)**2
-            dB=(B-eB)**2
-            dC=(C-eC)**2
-            dD=(D-eD)**2
-            dmax=max(dA,dB,dC,dD)
-            
-            if dmax==dA or dmax==dD:
-                match=allele["0"]+"-"+allele_n["0"]+","+allele["1"]+"-"+allele_n["1"]
-            else:
-                match=allele["0"]+"-"+allele_n["1"]+","+allele["1"]+"-"+allele_n["0"]
-            
-        else:
-            match=" - , - "
-    
-    else:
-        D_prime="--"
-        r2="--"
-        match=" - , - "
-    
-    return [D_prime,r2,match]
+	# Extract haplotypes
+	A=hap["00"]
+	B=hap["01"]
+	C=hap["10"]
+	D=hap["11"]
+	
+	N=A+B+C+D
+	delta=float(A*D-B*C)
+	Ms=float((A+C)*(B+D)*(A+B)*(C+D))
+	
+	# Minor allele frequency
+	maf_q=min((A+B)/float(N),(C+D)/float(N))
+	maf_p=min((A+C)/float(N),(B+D)/float(N))
+	
+	if Ms!=0:
+		
+		# D prime
+		if delta<0:
+			D_prime=delta/min((A+C)*(A+B),(B+D)*(C+D))
+		else:
+			D_prime=delta/min((A+C)*(C+D),(A+B)*(B+D))
+		
+		# R2
+		r2=(delta**2)/Ms
+		
+		# Find Correlated Alleles
+		if r2>0.1:
+			
+			# Expected Cell Counts
+			eA=(A+B)*(A+C)/N
+			eB=(B+A)*(B+D)/N
+			eC=(C+A)*(C+D)/N
+			eD=(D+C)*(D+B)/N
+			
+			# Calculate Deltas
+			dA=(A-eA)**2
+			dB=(B-eB)**2
+			dC=(C-eC)**2
+			dD=(D-eD)**2
+			dmax=max(dA,dB,dC,dD)
+			
+			if dmax==dA or dmax==dD:
+				match=allele["0"]+"-"+allele_n["0"]+","+allele["1"]+"-"+allele_n["1"]
+			else:
+				match=allele["0"]+"-"+allele_n["1"]+","+allele["1"]+"-"+allele_n["0"]
+			
+		else:
+			match=" - , - "
+	
+	else:
+		D_prime="--"
+		r2="--"
+		match=" - , - "
+	
+	return [maf_q,maf_p,D_prime,r2,match]
 
 
 # Open Connection to RegulomeDB
@@ -121,38 +125,38 @@ for i in range(9,len(head)):
 # Loop through SNPs
 out=[]
 for geno_n in vcf:
-    if geno_n[3] in ["A","C","G","T"] and geno_n[4] in ["A","C","G","T"]:
-        allele_n={"0":geno_n[3],"1":geno_n[4]}
-        hap={"00":0,"01":0,"10":0,"11":0}
-        for i in index:
-            hap0=geno[i][0]+geno_n[i][0]
-            hap1=geno[i][1]+geno_n[i][1]
-            hap[hap0]+=1
-            hap[hap1]=1
+	if geno_n[3] in ["A","C","G","T"] and geno_n[4] in ["A","C","G","T"]:
+		allele_n={"0":geno_n[3],"1":geno_n[4]}
+		hap={"00":0,"01":0,"10":0,"11":0}
+		for i in index:
+			hap0=geno[i][0]+geno_n[i][0]
+			hap1=geno[i][1]+geno_n[i][1]
+			hap[hap0]+=1
+			hap[hap1]=1
 
-        D_prime,r2,match=LD_calcs(hap,allele,allele_n)
-        
-        if r2!="--" and r2>0.0:
-            bp_n=geno_n[1]
-            rs_n=geno_n[2]
-            al_n="("+geno_n[3]+"/"+geno_n[4]+")"
-            dist=str(int(geno_n[1])-int(geno[1]))
-            
-            # Get RegulomeDB score
-            t=("chr"+geno_n[0]+":"+geno_n[1],)
-            curr.execute('SELECT * FROM regdb WHERE position=?', t)
-            score=curr.fetchone()
-            if score==None:
-                score="."
-            else:
-                score=score[2]
-            
-            temp=[rs,al,"chr"+chr+":"+bp,rs_n,al_n,"chr"+chr+":"+bp_n,dist,D_prime,r2,match,score]
-            out.append(temp)
+		maf_q,maf_p,D_prime,r2,match=LD_calcs(hap,allele,allele_n)
+		
+		if r2!="--" and r2>0.0:
+			bp_n=geno_n[1]
+			rs_n=geno_n[2]
+			al_n="("+geno_n[3]+"/"+geno_n[4]+")"
+			dist=str(int(geno_n[1])-int(geno[1]))
+			
+			# Get RegulomeDB score
+			t=("chr"+geno_n[0]+":"+geno_n[1],)
+			curr.execute('SELECT * FROM regdb WHERE position=?', t)
+			score=curr.fetchone()
+			if score==None:
+				score="."
+			else:
+				score=score[2]
+			
+			temp=[rs,al,"chr"+chr+":"+bp,rs_n,al_n,"chr"+chr+":"+bp_n,dist,D_prime,r2,match,score,maf_q,maf_p]
+			out.append(temp)
 
 output=open(request+"_"+process+".out","w")
 for i in range(len(out)):
-    print >> output, "\t".join(str(j) for j in out[i])
+	print >> output, "\t".join(str(j) for j in out[i])
 
 
 output.close()
