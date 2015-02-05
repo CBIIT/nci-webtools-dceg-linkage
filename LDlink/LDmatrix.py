f726770ce58f710b87925cf3d8e073d8d31155cc
machiela <machielamj@nciws-d275-v.nci.nih.gov>
2015-02-05 18:18:21 -0500

    Remove duplicate RS numbers

--- LDlink/LDmatrix.py
+++ LDlink/LDmatrix.py
@@ -24,14 +24,21 @@ def calculate_matrix(snplst,pop,request):
 
 
 	# Open SNP list file
-	snps=open(snplst).readlines()
-	if len(snps)>301:
-		output["error"]="Maximum SNP list is 100 RS numbers. Your list contains "+str(len(snps))+" entries."
+	snps_raw=open(snplst).readlines()
+	if len(snps_raw)>300:
+		output["error"]="Maximum SNP list is 300 RS numbers. Your list contains "+str(len(snps_raw))+" entries."
 		json_output=json.dumps(output, sort_keys=True, indent=2)
 		print >> out_json, json_output
 		out_json.close()
 		return("","")
 		raise
+	
+	# Remove duplicate RS numbers
+	snps=[]
+	for snp_raw in snps_raw:
+		snp=snp_raw.strip().split()
+		if snp not in snps:
+			snps.append(snp)
 
 
 	# Select desired ancestral populations
@@ -68,8 +75,7 @@ def calculate_matrix(snplst,pop,request):
 	snp_coords=[]
 	warn=[]
 	tabix_coords=""
-	for i in range(len(snps)):
-		snp_i=snps[i].strip().split()
+	for snp_i in snps:
 		if len(snp_i)>0:
 			if len(snp_i[0])>2:
 				if snp_i[0][0:2]=="rs":
