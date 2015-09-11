ee80c0bd7fc335ddcdb0fea6ac410c95c2fdacbd
machiela <machielamj@nciws-d275-v.nci.nih.gov>
2015-09-11 17:06:19 -0400

    clarify coordinates are GRCh37

--- LDlink/LDproxy.py
+++ LDlink/LDproxy.py
@@ -560,7 +560,7 @@ def calculate_proxy(snp,pop,request):
 					
 	gene_plot.segment(genes_plot_start, genes_plot_yn, genes_plot_end, genes_plot_yn, color="black", alpha=1, line_width=2)
 	gene_plot.rect(exons_plot_x, exons_plot_yn, exons_plot_w, exons_plot_h, source=source2, fill_color="grey", line_color="grey")
-	gene_plot.xaxis.axis_label="Chromosome "+snp_coord[1]+" Coordinate (Mb)"
+	gene_plot.xaxis.axis_label="Chromosome "+snp_coord[1]+" Coordinate (Mb)(GRCh37)"
 	gene_plot.yaxis.axis_label="Genes"
 	gene_plot.ygrid.grid_line_color=None
 	gene_plot.yaxis.axis_line_color=None
