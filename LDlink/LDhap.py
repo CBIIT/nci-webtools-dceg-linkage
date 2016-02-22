3f349776a5691cf8253322628b5493eff91bdc57
machiela <machielamj@nciws-d275-v.nci.nih.gov>
2016-02-22 17:21:47 -0500

    Fix search error when only one variant in list

--- LDlink/LDhap.py
+++ LDlink/LDhap.py
@@ -214,7 +214,7 @@ def calculate_hap(snplst,pop,request):
 		else:
 			count=-2
 			found="false"
-			while count<=2:
+			while count<=2 and count+g<len(vcf):
 				geno_next=vcf[g+count].strip().split()
 				if rs_query==geno_next[2]:
 					found="true"
