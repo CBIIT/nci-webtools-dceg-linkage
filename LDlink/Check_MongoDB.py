5faa0a760308c06e344b3e464c5c5cbf0e9552e0
park-brian <re.public@outlook.com>
2016-07-29 11:37:39 -0400

    added script to test mongoDB connection

---
+++ LDlink/Check_MongoDB.py
@@ -0,0 +1,23 @@
+from pymongo import *
+
+def test_mongo(filename):
+
+    try:
+        contents=open(filename).read().split('\n')
+        username=contents[0].split('=')[1]
+        password=contents[1].split('=')[1]
+        port=int(contents[2].split('=')[1])
+        client = MongoClient('localhost', port)
+
+    except ValueError, e:
+            return "Error reading" + filename, e
+
+    except pymongo.errors.ConnectionFailure:
+            print "MongoDB is down on port", port
+            print "syntax: mongod --dbpath /local/content/ldlink/mongo/data/db/ --auth"
+            return "Failed to connect to server. Please verify the contents of " + filename
+
+
+    return "connected to server"
+
+print(test_mongo("SNP_Query_loginInfo.ini"))
