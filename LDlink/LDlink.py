83b129dcec5808a5ef33a30e0aadebc5dfa374cb
yewu <wuye@mail.nih.gov>
2017-03-28 14:20:02 -0400

    Updated LDpair

--- LDlink/LDlink.py
+++ LDlink/LDlink.py
@@ -166,6 +166,7 @@ def ldpair():
     # python LDpair.py rs2720460 rs11733615 EUR 38
     # r1 = '{"corr_alleles":["rs2720460(A) allele is correlated with rs11733615(C) allele","rs2720460(G) allele is correlated with rs11733615(T) allele"],"haplotypes":{"hap1":{"alleles":"AC","count":"576","frequency":"0.573"},"hap2":{"alleles":"GT","count":"361","frequency":"0.359"},"hap3":{"alleles":"GC","count":"42","frequency":"0.042"},"hap4":{"alleles":"AT","count":"27","frequency":"0.027"}},"snp1":{"allele_1":{"allele":"A","count":"603","frequency":"0.599"},"allele_2":{"allele":"G","count":"403","frequency":"0.401"},"coord":"chr4:104054686","rsnum":"rs2720460"},"snp2":{"allele_1":{"allele":"C","count":"618","frequency":"0.614"},"allele_2":{"allele":"T","count":"388","frequency":"0.386"},"coord":"chr4:104157164","rsnum":"rs11733615"},"statistics":{"chisq":"738.354","d_prime":"0.8839","p":"0.0","r2":"0.734"},"two_by_two":{"cells":{"11":"576","12":"27","21":"42","22":"361"},"total":"1006"}'
 
+    isProgrammatic = False
     print 'Execute TEST'
     print 'Gathering Variables from url'
     var1 = request.args.get('var1', False)
@@ -176,6 +177,7 @@ def ldpair():
         reference = request.args.get('reference', False)
     else:
         reference = str(time.strftime("%I%M%S"))
+        isProgrammatic = True
 
     print 'var1: ' + var1
     print 'var2: ' + var2
@@ -186,6 +188,12 @@ def ldpair():
         out_json = calculate_pair(var1, var2, pop, reference)
     except:
         return sendTraceback()
+
+    if isProgrammatic:
+        fp = open('./tmp/LDpair_'+reference+'.txt', "r")
+        content = fp.read()
+        fp.close()
+        return content
 
     return current_app.response_class(out_json, mimetype='application/json')
 
