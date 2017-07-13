948fc6c1cf70653cb7dfb8c84b8a582e85824045
kjiang95 <kjiang95@umd.edu>
2017-07-13 12:50:45 -0400

    updated hoploreg link from v2 to v4 for all LDlink models [NCIATWP-765]

--- LDlink/LDlink.js
+++ LDlink/LDlink.js
@@ -799,7 +799,7 @@ function ldproxy_haploreg_link(data, type, row) {
 
     // Create RegulomeDB links
 
-    var server = 'http://www.broadinstitute.org/mammals/haploreg/detail_v2.php';
+    var server = 'http://www.broadinstitute.org/mammals/haploreg/detail_v4.1.php';
 
     var rs_number = row[0];
     var params = {
