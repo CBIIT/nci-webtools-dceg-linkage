64881b20dca0939f1980f700d321fa06cf6fa251
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-07-30 11:44:26 -0400

    fix indentation error in LDpair

--- LDlink/LDpair.py
+++ LDlink/LDpair.py
@@ -14,9 +14,9 @@ def calculate_pair(snp1, snp2, pop, request=None):
     # vcf_dir = data_dir + "1000G/Phase3/genotypes/ALL.chr"
 
     # Set data directories USING CONFIG
-	snp_dir=config.data['snp_dir']
-	pop_dir=config.data['pop_dir']
-	vcf_dir=config.data['vcf_dir']
+    snp_dir=config.data['snp_dir']
+    pop_dir=config.data['pop_dir']
+    vcf_dir=config.data['vcf_dir']
 
     tmp_dir = "./tmp/"
 
