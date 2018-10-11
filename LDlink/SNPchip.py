15a46b53a532b835b3cbd258a9def50cfeb79edc
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-10-11 11:51:11 -0400

    add check if new dbSNP position remains the same for SNPchip

--- LDlink/SNPchip.py
+++ LDlink/SNPchip.py
@@ -130,12 +130,7 @@ def calculate_chip(snplst, platform_query, request):
                         # if new dbSNP151 position is 1 off
                         rs_nums.append(snp_i[0])
                         snp_pos.append(str(int(snp_coord[2]) + 1))
-                        temp2 = [snp_i[0], chr, int(snp_coord[2]) + 1]
-                        snp_coords.append(temp2)
-                        # if new dbSNP151 position is the same
-                        rs_nums.append(snp_i[0])
-                        snp_pos.append(snp_coord[2])
-                        temp = [snp_i[0], chr, int(snp_coord[2])]
+                        temp = [snp_i[0], chr, int(snp_coord[2]) + 1]
                         snp_coords.append(temp)
                     else:
                         warn.append(snp_i[0])
