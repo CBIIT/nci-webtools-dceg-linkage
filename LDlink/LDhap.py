47fe56582b917b13417cc8c509476bcdb9fdbbe5
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-10-11 10:57:20 -0400

    add condition for edge case no 1-off rsnums

--- LDlink/LDhap.py
+++ LDlink/LDhap.py
@@ -120,15 +120,15 @@ def calculate_hap(snplst, pop, request):
                     print snp_coord
                     if snp_coord != None:
                         rs_nums.append(snp_i[0])
-                        # new dbSNP151 position is 1 off
+                        # if new dbSNP151 position is 1 off
                         snp_pos.append(str(int(snp_coord[2]) + 1))
-                        rs_nums.append(snp_i[0]) # duplicate
+                        rs_nums.append(snp_i[0])
                         snp_pos.append(snp_coord[2])
-                        # new dbSNP151 position is 1 off
-                        temp = [snp_i[0], snp_coord[1],
-                                str(int(snp_coord[2]) + 1)]
-                        print "TEMP"
-                        print temp
+                        # if new dbSNP151 position is 1 off
+                        temp2 = [snp_i[0], snp_coord[1],
+                                 str(int(snp_coord[2]) + 1)]
+                        snp_coords.append(temp2)
+                        temp = [snp_i[0], snp_coord[1], snp_coord[2]]
                         snp_coords.append(temp)
                     else:
                         warn.append(snp_i[0])
