49bf1272e6dd6c117ac31c6a964a801cf879f021
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-10-31 11:42:42 -0400

    prototype email sending for API registration

--- LDlink/RegisterAPI.py
+++ LDlink/RegisterAPI.py
@@ -7,7 +7,9 @@ import binascii
 import yaml
 
 import smtplib
-from email.message import EmailMessage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 # Set data directories using config.yml
@@ -17,25 +19,18 @@ api_users_dir = config['data']['api_users_dir']
 
 # email user token
 def emailUser(email, token):
-    # Open the plain text file whose name is in textfile for reading.
-    textfile = "EmailTemplate.txt"
-    with open(textfile) as fp:
-        # Create a text/plain message
-        msg = EmailMessage()
-        msg.set_content(fp.read())
+    print "sending message"
 
-    # me == the sender's email address
-    # you == the recipient's email address
-    me = "NCILDlinkWebAdmin@mail.nih.gov"
-    you = email
-    msg['Subject'] = 'The contents of %s' % textfile
-    msg['From'] = me
-    msg['To'] = you
+    packet = MIMEMultipart()
+    packet['Subject'] = "LDlink API Access"
+    packet['From'] = "LDlink" + " <do.not.reply@nih.gov>"
+    packet['To'] = email
 
-    # Send the message via our own SMTP server.
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
+    packet.attach(MIMEText("Test!", 'html'))
+
+    # print self.MAIL_HOST
+    smtp = smtplib.SMTP("localhost")
+    smtp.sendmail("do.not.reply@nih.gov", email, packet.as_string())
 
 # creates table in database if database did not exist before
 def createTable(api_users_dir):
