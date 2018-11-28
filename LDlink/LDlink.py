50462fd2e03110e517e2b98ef7588f6a1b274b69
Kevin Jiang <21976531+kjiang95@users.noreply.github.com>
2018-11-28 14:15:49 -0500

    add CORS to Flask

--- LDlink/LDlink.py
+++ LDlink/LDlink.py
@@ -909,7 +909,12 @@ def api_stats():
     out_json = getStats(startdatetime, enddatetime, top)
     return sendJSON(out_json)
 
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
