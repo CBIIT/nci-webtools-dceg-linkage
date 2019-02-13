76227f5fa198f49e25c8f5723ddfc746390228ed
kvnjng <kvvnjng@gmail.com>
2019-02-13 13:48:22 -0500

    prototype LDpop Google Maps API map

--- LDlink/LDlink.js
+++ LDlink/LDlink.js
@@ -3494,7 +3494,7 @@ function eraseCookie(name) {
 function initMap() {
     var chicago = new google.maps.LatLng(41.850, -87.650);
   
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('ldpop-map'), {
       center: chicago,
       zoom: 3
     });
