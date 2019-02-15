2395a6028dcfc82e857c35f78af6f2ab82e1709c
kvnjng <kvvnjng@gmail.com>
2019-02-15 13:13:15 -0500

    debug yml read gmp key

--- LDlink/LDlink.js
+++ LDlink/LDlink.js
@@ -2618,14 +2618,6 @@ function initMap(locations) {
         center: centerMap
     });
 
-    // var locations = [
-    //     ['Bondi Beach', -33.890542, 151.274856, 4],
-    //     ['Coogee Beach', -33.923036, 151.259052, 5],
-    //     ['Cronulla Beach', -34.028249, 151.157507, 3],
-    //     ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-    //     ['Maroubra Beach', -33.950198, 151.259302, 1]
-    // ];
-
     var infowindow = new google.maps.InfoWindow();
     var marker, i;
 
