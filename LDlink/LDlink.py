254dd87357256f174312ff07f9847fbf20225df5
kvnjng <kvvnjng@gmail.com>
2019-03-26 10:24:38 -0400

    refactor Flask route for LDpair

--- LDlink/LDlink.py
+++ LDlink/LDlink.py
@@ -604,44 +604,50 @@ def ldmatrix():
 @app.route('/LDlinkRestWeb/ldpair', methods=['GET'])
 @requires_token
 def ldpair():
-    # python LDpair.py rs2720460 rs11733615 EUR 38
-    # r1 = '{"corr_alleles":["rs2720460(A) allele is correlated with rs11733615(C) allele","rs2720460(G) allele is correlated with rs11733615(T) allele"],"haplotypes":{"hap1":{"alleles":"AC","count":"576","frequency":"0.573"},"hap2":{"alleles":"GT","count":"361","frequency":"0.359"},"hap3":{"alleles":"GC","count":"42","frequency":"0.042"},"hap4":{"alleles":"AT","count":"27","frequency":"0.027"}},"snp1":{"allele_1":{"allele":"A","count":"603","frequency":"0.599"},"allele_2":{"allele":"G","count":"403","frequency":"0.401"},"coord":"chr4:104054686","rsnum":"rs2720460"},"snp2":{"allele_1":{"allele":"C","count":"618","frequency":"0.614"},"allele_2":{"allele":"T","count":"388","frequency":"0.386"},"coord":"chr4:104157164","rsnum":"rs11733615"},"statistics":{"chisq":"738.354","d_prime":"0.8839","p":"0.0","r2":"0.734"},"two_by_two":{"cells":{"11":"576","12":"27","21":"42","22":"361"},"total":"1006"}'
-    web = False
-    if 'LDlinkRestWeb' in request.path:
-        web = True
-    else:
-        web = False
-    isProgrammatic = False
-    print 'Execute ldpair'
-    print 'Gathering Variables from url'
+    print 'Execute ldpair.'
     var1 = request.args.get('var1', False)
     var2 = request.args.get('var2', False)
     pop = request.args.get('pop', False)
-    # check if call is from API or Web instance by seeing if reference number has already been generated or not
-    # if accessed by web instance, generate reference number via javascript after hit calculate button
-    if request.args.get('reference', False):
-        reference = request.args.get('reference', False)
-    else:
-        reference = str(time.strftime("%I%M%S")) + `random.randint(0, 10000)`
-        isProgrammatic = True
+    token = request.args.get('token', False)
     print 'var1: ' + var1
     print 'var2: ' + var2
     print 'pop: ' + pop
-    print 'request: ' + str(reference)
-    try:
-        out_json = calculate_pair(var1, var2, pop, web, reference)
-        # if API call has error, retrieve error message from json returned from calculation
+    web = False
+    # differentiate web or api request
+    if 'LDlinkRestWeb' in request.path:
+        # WEB REQUEST
+        web = True
+        reference = request.args.get('reference', False)
+        print 'request: ' + str(reference)
         try:
-            if isProgrammatic:
-                fp = open('./tmp/LDpair_'+reference+'.txt', "r")
-                content = fp.read()
-                fp.close()
-                return content
+            out_json = calculate_pair(var1, var2, pop, web, reference)
         except:
-            output = json.loads(out_json)
-            return sendTraceback(output["error"])
-    except:
-        return sendTraceback(None)
+            return sendTraceback(None)
+    else:
+        # API REQUEST
+        web = False
+        reference = str(time.strftime("%I%M%S")) + `random.randint(0, 10000)`
+        print 'request: ' + str(reference)
+        try:
+            # lock token preventing concurrent requests
+            toggleLocked(token, 1)
+            out_json = calculate_pair(var1, var2, pop, web, reference)
+            # retrieve error message from json returned from calculation
+            try:
+                # unlock token then display api output
+                with open('./tmp/LDpair_'+reference+'.txt', "r") as fp:
+                    content = fp.read()
+                toggleLocked(token, 0)
+                return content
+            except:
+                # unlock token then display error message
+                output = json.loads(out_json)
+                toggleLocked(token, 0)
+                return sendTraceback(output["error"])
+        except:
+            # unlock token if internal error w/ calculation
+            toggleLocked(token, 0)
+            return sendTraceback(None)
     return current_app.response_class(out_json, mimetype='application/json')
 
 # Web and API route for LDpop
