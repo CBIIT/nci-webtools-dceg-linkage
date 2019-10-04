b2ebcd01c02c97b57c5b0a82d822c66dc09c0014
kvnjng <kvvnjng@gmail.com>
2019-10-04 12:31:01 -0400

    debug byte to str conversion for python 3

--- LDlink/LDhap.py
+++ LDlink/LDhap.py
@@ -24,9 +24,9 @@ def calculate_hap(snplst, pop, request, web):
     # snp_dir = config['data']['snp_dir']
     # snp_chr_dir = config['data']['snp_chr_dir']
     # snp_pos_offset = config['data']['snp_pos_offset']
-    dbsnp_version = config['data']['dbsnp_version']
-    pop_dir = config['data']['pop_dir']
-    vcf_dir = config['data']['vcf_dir']
+    dbsnp_version = config['data']['dbsnp_version'].decode('utf-8')
+    pop_dir = config['data']['pop_dir'].decode('utf-8')
+    vcf_dir = config['data']['vcf_dir'].decode('utf-8')
 
     tmp_dir = "./tmp/"
 
@@ -287,7 +287,7 @@ def calculate_hap(snplst, pop, request, web):
     pos_lst = []
 
     for g in range(h+1, len(vcf)):
-        geno = vcf[g].strip().split()
+        geno = vcf[g].decode('utf-8').strip().split()
         if geno[1] not in snp_pos:
             if "warning" in output:
                 output["warning"] = output["warning"]+". Genomic position ("+geno[1]+") in VCF file does not match db" + \
