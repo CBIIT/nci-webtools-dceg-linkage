3e4eaa87bc8397c11ff85951142a0094d1727a6b
kvnjng <kvvnjng@gmail.com>
2019-10-07 14:31:30 -0400

    debug fix ldhap api for python 3

--- LDlink/LDhap.py
+++ LDlink/LDhap.py
@@ -223,6 +223,8 @@ def calculate_hap(snplst, pop, request, web):
     # Import SNP VCF files
     vcf = [x.decode('utf-8') for x in proc.stdout.readlines()]
 
+    print("VCF", vcf)
+
     # Make sure there are genotype data in VCF file
     if vcf[-1][0:6] == "#CHROM":
         output["error"] = "No query variants were found in 1000G VCF file"
