71a006527053f075963d0f85fad6bdea048e2d02
kvnjng <kvvnjng@gmail.com>
2019-12-12 17:03:23 -0500

    create json file with problematic SNPs

--- LDlink/LDtrait_data.py
+++ LDlink/LDtrait_data.py
@@ -3,20 +3,19 @@ from datetime import datetime
 import requests
 import os
 import sys
+import json
 from pymongo import MongoClient, ASCENDING
 from pymongo.errors import ConnectionFailure
 import time
 import yaml
 
 
-# contents = open("/analysistools/public_html/apps/LDlink/app/SNP_Query_loginInfo.ini").read().split('\n')
-# username = 'ncianalysis_api'
-# password = contents[1].split('=')[1]
-# port = int(contents[2].split('=')[1])
-
 start_time = time.time()  # measure script's run time
+filename = "gwas_catalog_" + datetime.today().strftime('%Y-%m-%d') + ".tsv"
+errFilename = "ldtrait_error_snps_" + datetime.today().strftime('%Y-%m-%d') + ".json"
 
 # Load variables from config file
+# with open('/analysistools/public_html/apps/LDlink/app/config.yml', 'r') as c:
 with open('config.yml', 'r') as c:
     config = yaml.load(c)
 ldtrait_src = config['data']['ldtrait_src']
@@ -25,14 +24,8 @@ tmp_dir = config['data']['tmp_dir']
 if not os.path.exists(tmp_dir):
     os.makedirs(tmp_dir)
 
-# Load variables from config file
-with open('config.yml', 'r') as c:
-    config = yaml.load(c)
-ldtrait_src = config['data']['ldtrait_src']
-
 # download daily update of GWAS Catalog
 def downloadGWASCatalog():
-    filename = "gwas_catalog_" + datetime.today().strftime('%Y-%m-%d') + ".tsv"
     if (os.path.isfile(tmp_dir + filename)):
         print("Latest GWAS catalog already downloaded, deleting existing...")
         os.remove(tmp_dir + filename)
@@ -73,7 +66,6 @@ def main():
         gwas_catalog = db.gwas_catalog
 
     # read and insert downloaded file
-    filename = "gwas_catalog_" + datetime.today().strftime('%Y-%m-%d') + ".tsv"
     with open(tmp_dir + filename) as f:
         lines = f.readlines()
         headers = lines[0].strip().split('\t')
@@ -85,28 +77,31 @@ def main():
         print("Finding genomics coordinates from dbsnp and inserting to MongoDB collection...")
         no_dbsnp_match = 0
         problematic_gwas_variants = 0
+        errSNPs = []
         for line in lines:
             values = line.strip().split('\t')
+            values.append("NA")
+            values.append("NA")
+            document = dict(list(zip(headers, values)))
+            # check if orginal gwas row has populated rs number column
             if len(values[23]) > 0:
                 # find chr, pos in dbsnp using rsid
                 record = dbsnp.find_one({"id": values[23]})
-                # if found, add to chr, pos to record
+                # if found in dbsnp, add to chr, pos to record
                 if record is not None and len(record["chromosome"]) > 0 and len(record["position"]) > 0: 
-                    values.append(str(record["chromosome"]))
-                    values.append(int(record["position"]))
+                    document["chromosome_grch37"] = str(record["chromosome"])
+                    document["position_grch37"] = int(record["position"])
+                    gwas_catalog.insert_one(document)
                 else:
-                    values.append("NA")
-                    values.append("NA")
+                    document["err_msg"] = "Genomic coordinates not found in dbSNP."
+                    errSNPs.append(document)
                     no_dbsnp_match += 1
             else:
-                values.append("NA")
-                values.append("NA")
+                document["err_msg"] = "SNP missing valid RSID."
+                errSNPs.append(document)
                 problematic_gwas_variants += 1
-            document = dict(list(zip(headers, values)))
-            # if document["SNPS"][0:2] != "rs" and document["SNPS"][0:3] != "chr" and document["SNPS"][0:3] != "Chr":
-            #     print document["SNPS"]
-            # print document
-            gwas_catalog.insert_one(document)
+        with open(tmp_dir + errFilename, 'a') as errFile:
+            json.dump(errSNPs, errFile)
 
     print("Problematic GWAS variants:", problematic_gwas_variants)
     print("Genomic position not found in dbSNP:", no_dbsnp_match)
@@ -116,17 +111,7 @@ def main():
     print("Indexing GWAS catalog Mongo collection...")
     gwas_catalog.create_index([("chromosome_grch37", ASCENDING), ("position_grch37", ASCENDING)])
     print("Indexing completed.")
-
     print(("Completion time:\t--- %s minutes ---" % str(((time.time() - start_time) / 60.0))))
-
-
-    # with open('/analysistools/public_html/apps/LDlink/app/config.yml', 'r') as f:
-    #     config = yaml.load(f)
-    # api_mongo_addr = config['api']['api_mongo_addr']
-    # # client = MongoClient('mongodb://'+username+':'+password+'@'+api_mongo_addr+'/LDLink', port)
-    # client = MongoClient('mongodb://'+username+':'+password+'@localhost/LDLink', port)
-    # db = client["LDLink"]
-    # users = db.api_users
 
 if __name__ == "__main__":
     main()
