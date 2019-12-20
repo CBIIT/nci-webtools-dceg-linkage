1a0950c532d3a2c95b2094cdf7e7df76254e0ab8
kvnjng <kvvnjng@gmail.com>
2019-12-20 15:27:17 -0500

    debug NaN

--- LDlink/LDlink.js
+++ LDlink/LDlink.js
@@ -744,27 +744,27 @@ function createTraitDetailsTable() {
         "bProcessing": false,
         "deferRender": false,
         "columnDefs": [
-            {
-                "render": function ( data, type, row ) {
-                    // Round R2 float to 4 decimal places
-                    return parseFloat(data).toFixed(4);
-                },
-                "targets": 4
-            },
-            {
-                "render": function ( data, type, row ) {
-                    // Round D' float to 4 decimal places
-                    return parseFloat(data).toFixed(4);
-                },
-                "targets": 5
-            },
-            {
-                "render": function ( data, type, row ) {
-                    // Round P-value float to 4 decimal places
-                    return parseFloat(data).toFixed(4);
-                },
-                "targets": 9
-            },
+            // {
+            //     "render": function ( data, type, row ) {
+            //         // Round R2 float to 4 decimal places
+            //         return parseFloat(data).toFixed(4);
+            //     },
+            //     "targets": 4
+            // },
+            // {
+            //     "render": function ( data, type, row ) {
+            //         // Round D' float to 4 decimal places
+            //         return parseFloat(data).toFixed(4);
+            //     },
+            //     "targets": 5
+            // },
+            // {
+            //     "render": function ( data, type, row ) {
+            //         // Round P-value float to 4 decimal places
+            //         return parseFloat(data).toFixed(4);
+            //     },
+            //     "targets": 9
+            // },
             {
                 "render": function ( data, type, row ) {
                     // Provide link to LDpair 
