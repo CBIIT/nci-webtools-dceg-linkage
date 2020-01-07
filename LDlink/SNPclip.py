41843864e260693c6c0e69d603ce8e2bf5b624eb
kvnjng <kvvnjng@gmail.com>
2020-01-07 15:05:19 -0500

    connect to SNPclip data remotely from local

--- LDlink/SNPclip.py
+++ LDlink/SNPclip.py
@@ -29,6 +29,7 @@ def calculate_clip(snplst, pop, request, web, r2_threshold=0.1, maf_threshold=0.
     with open('config.yml', 'r') as f:
         config = yaml.load(f)
     env = config['env']
+    api_mongo_addr = config['api']['api_mongo_addr']
     dbsnp_version = config['data']['dbsnp_version']
     pop_dir = config['data']['pop_dir']
     vcf_dir = config['data']['vcf_dir']
@@ -84,15 +85,20 @@ def calculate_clip(snplst, pop, request, web, r2_threshold=0.1, maf_threshold=0.
     # Connect to Mongo snp database
     if env == 'local':
         contents = open("SNP_Query_loginInfo_test.ini").read().split('\n')
+        mongo_host = api_mongo_addr
     else: 
         contents = open("SNP_Query_loginInfo.ini").read().split('\n')
+        mongo_host = 'localhost'
     username = contents[0].split('=')[1]
     password = contents[1].split('=')[1]
     port = int(contents[2].split('=')[1])
     if web:
-        client = MongoClient('mongodb://'+username+':'+password+'@localhost/admin', port)
+        client = MongoClient('mongodb://'+username+':'+password+'@'+mongo_host+'/admin', port)
     else:
-        client = MongoClient('localhost', port)
+        if env == 'local':
+            client = MongoClient('mongodb://'+username+':'+password+'@'+mongo_host+'/admin', port)
+        else:
+            client = MongoClient('localhost', port)
     db = client["LDLink"]
 
     def get_coords(db, rsid):
