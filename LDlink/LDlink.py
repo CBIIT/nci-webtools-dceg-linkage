c865d86f90aafcaa3c0259e529e956a842cc2f80
kvnjng <kvvnjng@gmail.com>
2020-01-13 16:10:22 -0500

    fix URL in API error messages

--- LDlink/LDlink.py
+++ LDlink/LDlink.py
@@ -123,10 +123,14 @@ def requires_token(f):
         # Set data directories using config.yml
         with open('config.yml', 'r') as c:
             config = yaml.load(c)
+        env = config['env']
+        if env == 'local':
+            url_root = 'http://localhost:5000/'
+        elif env == 'prod':
+            url_root = 'https://ldlink.nci.nih.gov/'
+        else:
+            url_root = 'https://ldlink' + env + '.nci.nih.gov/'
         require_token = bool(config['api']['require_token'])
-        url_root = request.url_root
-        new_url_root = url_root.replace('http://', 'https://')
-        # api_access_dir = config['api']['api_access_dir']
         token_expiration = bool(config['api']['token_expiration'])
         token_expiration_days = config['api']['token_expiration_days']
         if ("LDlinkRestWeb" not in request.full_path):
@@ -134,11 +138,11 @@ def requires_token(f):
             if require_token:
                 # Check if token argument is missing in api call
                 if 'token' not in request.args:
-                    return sendTraceback('API token missing. Please register using the API Access tab: ' + new_url_root + '?tab=apiaccess')
+                    return sendTraceback('API token missing. Please register using the API Access tab: ' + url_root + '?tab=apiaccess')
                 token = request.args['token']
                 # Check if token is valid
                 if checkToken(token, token_expiration, token_expiration_days) is False or token is None:
-                    return sendTraceback('Invalid or expired API token. Please register using the API Access tab: ' + new_url_root + '?tab=apiaccess')
+                    return sendTraceback('Invalid or expired API token. Please register using the API Access tab: ' + url_root + '?tab=apiaccess')
                 # Check if token is blocked
                 if checkBlocked(token):
                     return sendTraceback("Your API token has been blocked. Please contact system administrator: NCILDlinkWebAdmin@mail.nih.gov")
