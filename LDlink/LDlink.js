10ce618cb75491c522b5768f36031442264c831b
Shen <shenbn@nci-02134517-ml.nih.gov>
2020-01-15 15:53:17 -0500

    changed the LDmatrix

--- LDlink/LDlink.js
+++ LDlink/LDlink.js
@@ -2513,13 +2513,15 @@ function updateLDmatrix() {
     var snps = DOMPurify.sanitize($('#' + id + '-file-snp-numbers').val());
     var population = getPopulationCodes(id+'-population-codes');
     var r2_d;
+    $("#ldmatrix-legend-r2").attr('src', 'LDmatrix_legend_R2.png');
+    $("#ldmatrix-legend-d").attr('src', 'LDmatrix_legend_Dprime_Blue.png');
 
     if($('#matrix_color_r2').hasClass('active')) {
         r2_d='r2'; // i.e. R2
-        $("#ldmatrix-legend").attr('src', 'LDmatrix_legend_R2.png');
+        //$("#ldmatrix-legend").attr('src', 'LDmatrix_legend_R2.png');
     } else {
         r2_d='d';  // i.e.  Dprime
-        $("#ldmatrix-legend").attr('src', 'LDmatrix_legend_Dprime.png');
+        //$("#ldmatrix-legend").attr('src', 'LDmatrix_legend_Dprime.png');
     }
 
     var ldmatrixInputs = {
