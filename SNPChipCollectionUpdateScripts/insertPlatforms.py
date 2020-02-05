a5929bc27113cc08002402be084e794b29fbff15
Shen <shenbn@nci-02134517-ml.nih.gov>
2020-02-05 17:25:47 -0500

    updated code names

--- SNPChipCollectionUpdateScripts/insertPlatforms.py
+++ SNPChipCollectionUpdateScripts/insertPlatforms.py
@@ -4,8 +4,8 @@ client = MongoClient()
 print(client)
 db = client['LDLink']
 collections = db['platforms']
-collections.insert_one({'platform':'Illumina Global Screening Array version 1', 'code':'C1'})
-collections.insert_one({'platform':'Illumina Global Screening Array version 2', 'code':'A1'})
-collections.insert_one({'platform':'Illumina Multi-Ethnic Global Array', 'code':'D1'})
-collections.insert_one({'platform':'Affymetrix Axiom UK Biobank Array', 'code':'UKB_WCSG'})
-collections.insert_one({'platform':'Affymetrix Axiom Precision Medicine Research Array', 'code':'PMRA'})
+collections.insert_one({'platform':'Illumina Global Screening Array version 1', 'code':'I_GSA-v1'})
+collections.insert_one({'platform':'Illumina Global Screening Array version 2', 'code':'I_GSA-v2'})
+collections.insert_one({'platform':'Illumina Multi-Ethnic Global Array', 'code':'I_MEGA'})
+collections.insert_one({'platform':'Affymetrix Axiom UK Biobank Array', 'code':'A_UKBA'})
+collections.insert_one({'platform':'Affymetrix Axiom Precision Medicine Research Array', 'code':'A_PMRA'})
