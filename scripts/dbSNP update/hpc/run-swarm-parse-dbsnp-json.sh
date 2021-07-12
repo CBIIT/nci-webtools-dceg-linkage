f663d40fc48ac1ac32f17c25ddeaca674e480ace
kvnjng <kvvnjng@gmail.com>
2021-07-12 01:19:59 -0500

    capture grch37 and 38 positions test

--- scripts/dbSNP update/hpc/run-swarm-parse-dbsnp-json.sh
+++ scripts/dbSNP update/hpc/run-swarm-parse-dbsnp-json.sh
@@ -72,4 +72,4 @@ done
 # --verbose <0-6> = choose verbose level, 6 being the most chatty
 # --gres=lscratch:<#> = number of gb of tmp space for each process subjob
 # swarm -f $SWARM_FILE -t 8 -g 64 --time 48:00:00 --verbose 6 --gres=lscratch:300 --logdir $LOG_PATH --module mysql/8.0,nodejs --merge-output
-# swarm -f $SWARM_FILE -t 2 -g 16 --time 48:00:00 --verbose 6 --gres=lscratch:200 --logdir $LOG_PATH --merge-output
+swarm -f $SWARM_FILE -t 2 -g 16 --time 48:00:00 --verbose 6 --gres=lscratch:200 --logdir $LOG_PATH --merge-output
--- scripts/dbSNP update/rsjson_mongo_filter.py
+++ scripts/dbSNP update/rsjson_mongo_filter.py
@@ -1,9 +1,12 @@
 import sys
 import json
-import gzip
+import bz2
+import shutil
 # from pymongo import MongoClient, ASCENDING
 # from pymongo.errors import ConnectionFailure
 import time
+import os.path
+import re
 start_time = time.time()  # measure script's run time
 
 
@@ -20,8 +23,12 @@ def getRSIDs(primary_refsnp):
 
 
 # find chromosome
-def getChromosome(f_in):
-    return f_in.name.split('.')[0].split('-')[1][3:]
+def getChromosome(file_basename):
+    try:
+        chromosome = file_basename.split('.')[0].split('-')[1].split('chr')[1] or "TEST"
+    except IndexError:
+        chromosome = "TEST"
+    return chromosome   
 
 
 # find sequence change annotations
@@ -48,84 +55,131 @@ def getVariantType(primary_refsnp):
 
 
 # find GRCh37 genomic position
-def getPosition(primary_refsnp, variant_type):
-    position = ''
-    for i in primary_refsnp['placements_with_allele']:
-        if len(i['placement_annot']['seq_id_traits_by_assembly']) > 0:
-            assembly = i['placement_annot']['seq_id_traits_by_assembly'][0]['assembly_name']
-            is_chrom = i['placement_annot']['seq_id_traits_by_assembly'][0]['is_chromosome']
-            pos = i['alleles'][0]['allele']['spdi']['position']
-            ########    
-            # only choose bp from GRCh37.p13
-            if is_chrom == True and assembly == "GRCh37.p13":
-                if variant_type == "delins":
-                    position = str(pos)
-                elif variant_type == "del":
-                    position = str(pos)
-                elif variant_type == "ins":
-                    position = str(pos)
-                else:
-                    position = str(int(pos) + 1)
-            ########  
-            ########
-            # CONSIDER PARSING "hgvs" property for 'corrected' position
-            ########  
-                
-    return position
+def getPositions(primary_refsnp, variant_type):
+    try:
+        for i in primary_refsnp['placements_with_allele']:
+            if len(i['placement_annot']['seq_id_traits_by_assembly']) > 0:
+                assembly = i['placement_annot']['seq_id_traits_by_assembly'][0]['assembly_name']
+                is_chrom = i['placement_annot']['seq_id_traits_by_assembly'][0]['is_chromosome']
+                pos = i['alleles'][0]['allele']['spdi']['position']
+
+                ######## parse position from GRCh37.p13
+                if is_chrom == True and assembly == "GRCh37.p13":
+                    if variant_type == "delins":
+                        position_grch37 = str(pos)
+                    elif variant_type == "del":
+                        position_grch37 = str(pos)
+                    elif variant_type == "ins":
+                        position_grch37 = str(pos)
+                    else:
+                        position_grch37 = str(int(pos) + 1)
+                    position_hgvs_grch37 = re.sub(r"\D", "", str(i['alleles'][0]['hgvs'].split(':')[1].split('.')[1]))
+
+                ######## parse position from GRCh38.p13
+                if is_chrom == True and assembly == "GRCh38.p13":
+                    if variant_type == "delins":
+                        position_grch38 = str(pos)
+                    elif variant_type == "del":
+                        position_grch38 = str(pos)
+                    elif variant_type == "ins":
+                        position_grch38 = str(pos)
+                    else:
+                        position_grch38 = str(int(pos) + 1)
+                    position_hgvs_grch38 = re.sub(r"\D", "", str(i['alleles'][0]['hgvs'].split(':')[1].split('.')[1]))
+    except:
+        position_grch37 = '',
+        position_hgvs_grch37 = '',
+        position_grch38 = '',
+        position_hgvs_grch38 = '',
+
+    return {
+        "position_grch37": position_grch37,
+        "position_hgvs_grch37": position_hgvs_grch37,
+        "position_grch38": position_grch38,
+        "position_hgvs_grch38": position_hgvs_grch38,
+    }
 
 
 # write output from parsing json files
-def createRecord(rsids, chromosome, position, annotations, variant_type, ref_id):
+def createRecord(rsids, chromosome, positions, annotations, variant_type, ref_id, tmp_path, out_path, result_file):
     if len(rsids) > 0:
         for rsid in rsids:
-            if len(rsid) > 0 and len(chromosome) > 0 and len(position) > 0 and len(annotations) > 0 and len(variant_type) > 0:
-                writeJSON(rsid, chromosome, position, ','.join(annotations), variant_type, ref_id)
+            if len(rsid) > 0 and len(chromosome) > 0 and len(positions['position_grch37']) > 0 and len(positions['position_hgvs_grch37']) > 0 and len(positions['position_grch38']) > 0 and len(positions['position_hgvs_grch38']) > 0 and len(annotations) > 0 and len(variant_type) > 0:
+                writeJSON(rsid, chromosome, positions, ','.join(annotations), variant_type, ref_id, tmp_path, out_path, result_file)
                 # insertMongoDB(rsid, chromosome, position, ','.join(annotations), variant_type)
-            elif len(rsid) > 0 and len(chromosome) > 0 and len(position) > 0 and len(annotations) == 0 and len(variant_type) > 0:
+            elif len(rsid) > 0 and len(chromosome) > 0 and len(positions) > 0 and len(annotations) == 0 and len(variant_type) > 0:
                 # if no annotations, insert NA
-                writeJSON(rsid, chromosome, position, 'NA', variant_type, ref_id)
+                writeJSON(rsid, chromosome, positions, 'NA', variant_type, ref_id, tmp_path, tmp_path, out_path, result_file)
                 # insertMongoDB(rsid, chromosome, position, 'NA', variant_type)
             else:
                 pass
 
 
-def writeJSON(rsid, chromosome, position, annotations, variant_type, ref_id):
+def writeJSON(rsid, chromosome, positions, annotations, variant_type, ref_id, tmp_path, out_path, result_file):
     record = {
         "id": rsid,
         "chromosome": chromosome,
-        "position": position,
+        "position_grch37": positions['position_grch37'],
+        "position_hgvs_grch37": positions['position_hgvs_grch37'],
+        "position_grch38": positions['position_grch38'],
+        "position_hgvs_grch38": positions['position_hgvs_grch38'],
         "function": annotations,
         "type": variant_type,
         "ref_id": ref_id
     }
-    with open('chr_' + chromosome + '_filtered.json', 'a') as outfile:
+    with open(os.path.join(out_path, result_file) if tmp_path is None else os.path.join(tmp_path, result_file), 'a') as outfile:
         json.dump(record, outfile)
         outfile.write('\n')
 
 
 def main():
-    print "Start creating json file(s) with filtered records. db: LDLink, collection: dbsnp151"
-    filename = sys.argv[1]
-    print filename
-    input_dir = 'json_refsnp/'
-    with gzip.open(input_dir + filename, 'rb') as f_in:
+    file_path = sys.argv[1]
+    out_path = sys.argv[2]
+    tmp_path = sys.argv[3] if len(sys.argv) > 3 else None
+
+    print("Starting to parse " + file_path + "...")
+
+    print("file_path", file_path)
+    print("out_path", out_path)
+    print("tmp_path", tmp_path)
+
+    file_basename = os.path.basename(file_path)
+
+    # copy file to hpc tmp scratch space if specified
+    if tmp_path is not None:
+        print("Copying data to tmp scratch space...")
+        shutil.copy(file_path, tmp_path)
+
+    print("Begin parsing...")
+    with bz2.open(file_path if tmp_path is None else os.path.join(tmp_path, file_basename), 'rb') as f_in:
         # limit lines read per file
         # cnt = 0
         for line in f_in:
             rs_obj = json.loads(line.decode('utf-8'))
+            # print("rs_obj", rs_obj)
             if 'primary_snapshot_data' in rs_obj:
                 rsids, ref_id = getRSIDs(rs_obj)
-                chromosome = getChromosome(f_in)
+                chromosome = getChromosome(file_basename)
+                result_file = 'chr_' + chromosome + '_filtered.json'
                 annotations = getAnnotations(rs_obj['primary_snapshot_data'])
                 variant_type = getVariantType(rs_obj['primary_snapshot_data'])
-                position = getPosition(rs_obj['primary_snapshot_data'], variant_type)
-                createRecord(rsids, chromosome, position, annotations, variant_type, ref_id)
-                # limit lines read per file
-                # cnt = cnt + 1
-                # if (cnt > 10):
-                #     break
-    print "JSON file(s) completed."
+                positions = getPositions(rs_obj['primary_snapshot_data'], variant_type)
+                createRecord(rsids, chromosome, positions, annotations, variant_type, ref_id, tmp_path, out_path, result_file)
+    #             # limit lines read per file
+    #             # cnt = cnt + 1
+    #             # if (cnt > 10):
+    #             #     break
+            else:
+                # ERROR PIPE
+                print("ERROR OBJ", rs_obj)
 
+    print("Parsing completed...")
+
+    # copy parsed results json from hpc tmp scratch space to output dir if specified
+    if tmp_path is not None:
+        print("Copying results from tmp scratch space to output directory...")
+        shutil.copy(os.path.join(tmp_path, result_file), out_path)
+    
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
