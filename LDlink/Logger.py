58793d893a777baaa2a0bb0cdf58e68114ccb62d
Ryan Damico <damicojr@gmail.com>
2021-10-18 12:41:10 -0400

    logger improvements

--- LDlink/Logger.py
+++ LDlink/Logger.py
@@ -3,17 +3,39 @@ import yaml
 import logging
 import os
 import sys
+from logging.handlers import TimedRotatingFileHandler
 
 # retrieve config
 with open('config.yml', 'r') as f:
     config = yaml.load(f)
 aws_info = config['aws']
 
-logging.basicConfig(filename="logs/ldlink.log", 
+logFilename = config['log']['filename']
+logLevel = config['log']['log_level']
+
+if (logLevel == 'DEBUG'):
+    logLevel = logging.DEBUG
+elif (logLevel == 'INFO'):
+    logLevel = logging.INFO
+elif (logLevel == 'WARNING'):
+    logLevel = logging.WARNING
+elif (logLevel == 'ERROR'):
+    logLevel = logging.ERROR
+elif (logLevel == 'CRITICAL'):
+    logLevel = logging.CRITICAL
+else:
+    logLevel = logging.DEBUG
+
+logging.basicConfig(filename=logFilename, 
                 format='%(levelname)s : %(asctime)s : %(message)s', 
-                filemode='w', level=logging.INFO, datefmt='%m-%d-%Y %I:%M:%S')
+                filemode='w', level=logLevel, datefmt='%m-%d-%Y %I:%M:%S')
 log = logging.getLogger("LDLink")
 #log.propagate = False
+
+#handler to rotate log file at specified time
+handler = TimedRotatingFileHandler(logFilename, when="midnight", interval=1)
+handler.suffix = "%Y%m%d"
+log.addHandler(handler)
 
 def logDebug(message):
     log.debug(message)
