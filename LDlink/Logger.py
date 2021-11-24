38cdc17ab0191f9e449625b4f6bc44a77967b916
kvnjng <kvvnjng@gmail.com>
2021-11-24 11:40:44 -0800

    remove custom suffix

--- LDlink/Logger.py
+++ LDlink/Logger.py
@@ -1,6 +1,7 @@
 import yaml
 import logging
 import logging.handlers
+# import re
 
 with open('config.yml', 'r') as f:
     config = yaml.load(f)
@@ -10,6 +11,15 @@ log_filename = config['log']['filename']
 log_level = config['log']['log_level']
 
 logger = logging.getLogger('LDlink')
+
+# Add the log message handler to the logger
+handler = logging.handlers.TimedRotatingFileHandler(log_dir + log_filename, when='M',  interval=10)
+# handler.suffix = "%y-%m-%d_%H:%M:%S"
+# handler.extMatch = re.compile(r"^\d{4}$")
+logFormatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%m-%d-%Y %I:%M:%S')
+handler.setFormatter(logFormatter)
+
+logger.addHandler(handler)
 
 if (log_level == 'DEBUG'):
     logger.setLevel(logging.DEBUG)
@@ -23,11 +33,3 @@ elif (log_level == 'CRITICAL'):
     logger.setLevel(logging.CRITICAL)
 else:
     logger.setLevel(logging.DEBUG)
-
-# Add the log message handler to the logger
-handler = logging.handlers.TimedRotatingFileHandler(log_dir + log_filename, when='M',  interval=10)
-handler.suffix = "%m-%d-%y_%H:%M:%S"
-logFormatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%m-%d-%Y %I:%M:%S')
-handler.setFormatter(logFormatter)
-
-logger.addHandler(handler)
