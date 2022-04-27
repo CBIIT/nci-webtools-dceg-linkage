c45755eafc97579091d594911b302b8d8d03d230
Judy Yao <xiaozheng.yao@nih.gov>
2022-04-27 13:37:21 -0400

    add ldassoc_example_dir for config

--- LDlink/LDutilites.py
+++ LDlink/LDutilites.py
@@ -11,12 +11,14 @@ def get_config():
     param_list['env'] = config['env']
     param_list['dbsnp_version'] = config['data']['dbsnp_version']
     param_list['population_samples_dir'] = config['data']['population_samples_dir']
+    param_list['ldassoc_example_dir'] =  config['data']['ldassoc_example_dir'] 
     param_list['data_dir'] = config['data']['data_dir']
     param_list['tmp_dir'] = config['data']['tmp_dir']
     param_list['genotypes_dir'] = config['data']['genotypes_dir']
     param_list['ldtrait_src'] = config['data']['ldtrait_src']
     param_list['aws_info'] = config['aws']
     param_list['num_subprocesses'] = config['performance']['num_subprocesses']
+    
    
     return (param_list)
 
