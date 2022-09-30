e9a30a9cc68b0d62f85fb0a13c79fea9df2bb079
Judy Yao <xiaozheng.yao@nih.gov>
2022-09-30 09:59:40 -0400

    turn off ldtrait long list message

--- LDlink/LDtrait.py
+++ LDlink/LDtrait.py
@@ -297,17 +297,17 @@ def calculate_trait(snplst, pop, request, web, r2_d, genome_build, r2_d_threshol
                 
     ldPairsUnique = [list(x) for x in set(tuple(x) for x in ldPairs)]
     print("length:",len(ldPairsUnique))
-    numSplit = math.ceil(len(ldPairsUnique)/4000)
-    if len(ldPairsUnique) > 4000:
+    #numSplit = math.ceil(len(ldPairsUnique)/4000)
+    #if len(ldPairsUnique) > 4000:
         #str(numSplit)
-        output["warning"] = 'Too many pairs found within window for input list and might cause timeout of the server.'\
-            ' Please click "Continue" if you want the calculation process and if server is finally timeout,'\
-            ' please split the input list as small lists and retry each sub list. ' + \
-                              str(output["warning"] if "warning" in output else "")
-        json_output = json.dumps(output, sort_keys=True, indent=2)
-        print(json_output, file=out_json)
-        out_json.close()
-        return("", "", "")
+    #    output["warning"] = 'Too many pairs found within window for input list and might cause timeout of the server.'\
+    #        ' Please click "Continue" if you want the calculation process and if server is finally timeout,'\
+    #        ' please split the input list as small lists and retry each sub list. ' + \
+    #                          str(output["warning"] if "warning" in output else "")
+    #    json_output = json.dumps(output, sort_keys=True, indent=2)
+    #    print(json_output, file=out_json)
+    #    out_json.close()
+    #    return("", "", "")
     snp_coords_gwas_unique = [list(x) for x in set(tuple(x) for x in ldPairs)]
     #print(snp_coords_gwas)	
     # print("ldPairsUnique", ldPairsUnique)	
