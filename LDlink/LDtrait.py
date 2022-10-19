3c1fb90a3b0a1373991b1b9986fdd0c7ec4b2730
Judy Yao <xiaozheng.yao@nih.gov>
2022-10-19 17:02:07 -0400

    modify timeout message ldtrait

--- LDlink/LDtrait.py
+++ LDlink/LDtrait.py
@@ -303,8 +303,8 @@ def calculate_trait(snplst, pop, request, web, r2_d, genome_build, r2_d_threshol
         #str(numSplit)
         output["warning"] = 'Too many pairs found within window for input list and might cause timeout of the server.'\
             ' Please click "Cancel", split the input list as small lists and retry each sub list. ' + \
-            ' If you still want to calculate current list, click "Continue" and if server is finally timeout,'\
-            ' please split the input list' + \
+            ' If you still want to calculate current list, click "Continue" and if error occurs,'\
+            ' please split the input list to retry each sub list' + \
                               str(output["warning"] if "warning" in output else "")
         json_output = json.dumps(output, sort_keys=True, indent=2)
         print(json_output, file=out_json)
