54dcd869e1fbd07bfeb8d7775d59872ce7aef6bb
Judy Yao <xiaozheng.yao@nih.gov>
2024-12-02 11:03:35 -0500

    initial resultStringCanvas

--- client/LDlink.js
+++ client/LDlink.js
@@ -3789,7 +3789,7 @@ function updateLDscore() {
 
     var dataString = data[0];
     var dataCanvas = [dataString, data[1]];
-
+    resultStringCanvas = "";
     // Find the index of the substring "Summary of LD Scores"
     var index = data.result.indexOf("Summary of LD Scores");
     if (index !== -1) {
