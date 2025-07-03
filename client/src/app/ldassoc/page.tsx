2dc41d9e246ae70488604d47132f17edfc23189b
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-07-03 10:15:41 -0400

    use common tool banner

--- client/src/app/ldassoc/page.tsx
+++ client/src/app/ldassoc/page.tsx
@@ -7,7 +7,7 @@ import Alert from "react-bootstrap/Alert";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import LDAssocForm from "./form";
 import CalculateLoading from "@/components/calculateLoading";
-import { useStore } from "@/store";
+import ToolBanner from "@/components/toolBanner";
 
 const LdAssocResults = dynamic(() => import("./results"), {
   ssr: false,
@@ -16,50 +16,25 @@ const LdAssocResults = dynamic(() => import("./results"), {
 export default function LdAssoc() {
   const searchParams = useSearchParams();
   const ref = searchParams.get("ref");
-  const genome_build = useStore((state) => state.genome_build);
-  const setGenomeBuild = useStore((state) => state.setGenomeBuild);
 
   return (
-    <Container fluid="md">
-      <Row className="border rounded bg-white my-3 p-3 shadow-sm">
-        <Col sm="2">
-          <Form.Group controlId="genome_build" className="mb-3">
-            <Form.Label>Genome Build (1000G)</Form.Label>
-            <Form.Select value={genome_build} onChange={(e) => setGenomeBuild(e.target.value)}>
-              <option value="grch37">GRCh37</option>
-              <option value="grch38">GRCh38</option>
-              <option value="grch38_high_coverage">GRCh38 High Coverage</option>
-            </Form.Select>
-          </Form.Group>
-        </Col>
-        <Col>
-          <h2 className="text-center ">
-            LDassoc Tool
-            <sup>
-              <a
-                href="/docs/#LDassoc"
-                target="_blank"
-                style={{ fontSize: 20, textDecoration: "none", marginLeft: 5 }}
-                title="Click here for documentation">
-                <i className="bi bi-info-circle-fill text-primary"></i>
-              </a>
-            </sup>
-          </h2>
-          <p className="text-center">
-            Interactively visualize association p-value results and linkage disequilibrium patterns for a genomic region
-            of interest.
-          </p>
-        </Col>
-        <Col sm="2" />
-      </Row>
-      <Row className="border rounded bg-white my-3 p-3 shadow-sm">
-        <Col>
-          <LDAssocForm />
-          <ErrorBoundary errorComponent={() => <Alert variant="warning">Error loading results</Alert>}>
-            <Suspense fallback={<CalculateLoading />}>{ref && <LdAssocResults ref={ref} />}</Suspense>
-          </ErrorBoundary>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <ToolBanner
+        name="LDassoc Tool"
+        href="/docs/#LDassoc"
+        description="Interactively visualize association p-value results and linkage disequilibrium patterns for a genomic
+              region of interest."
+      />
+      <Container fluid="md">
+        <Row className="border rounded bg-white my-3 p-3 shadow-sm">
+          <Col>
+            <LDAssocForm />
+            <ErrorBoundary errorComponent={() => <Alert variant="warning">Error loading results</Alert>}>
+              <Suspense fallback={<CalculateLoading />}>{ref && <LdAssocResults ref={ref} />}</Suspense>
+            </ErrorBoundary>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
