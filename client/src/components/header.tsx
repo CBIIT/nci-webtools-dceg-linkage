a431b0f41591b74bee0cdf27959a99da608f5b26
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2025-07-07 13:16:43 -0400

    FIxing 508 on header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -33,79 +33,74 @@ export function Header({ routes = [] }: HeaderProps) {
   const handleClick = () => newSearch();
 
   return (
-    <header>
-      <Container className="my-2">
-        <Row>
-          <div className="header">
-            <div className="container">
-              <header className="usa-banner__header">
-                <div className="usa-banner__inner">
-                  <div className="container">
-                    <div className="usa-banner__header-text">
-                      An official website of the United States government
-                    </div>
-                  </div>
-                </div>
-              </header>              
+    <>
+      <header className="usa-banner__header">
+        <div className="usa-banner__inner">
+          <div className="container">
+            <div className="usa-banner__header-text">
+              An official website of the United States government
             </div>
           </div>
-        </Row>
+        </div>
+      </header>
 
-        <Row>
-          <Col xl="9" md="8" sm="12">
-            <a rel="noopener noreferrer" href="https://cancercontrol.cancer.gov/">
-              <Image
-                src="/images/NIH-LDlink-Logo.png"
-                alt="LDlink Logo"
-                className="mw-100 ldlink-logo"
-                width={359.078}
-                height={90}
-                unoptimized
-              />
-            </a>
-          </Col>
+      <header>
+        <Container className="my-2">
+          <Row>
+            <Col xl="9" md="8" sm="12">
+              <a rel="noopener noreferrer" href="https://cancercontrol.cancer.gov/">
+                <Image
+                  src="/images/NIH-LDlink-Logo.png"
+                  alt="LDlink Logo"
+                  className="mw-100 ldlink-logo"
+                  width={359.078}
+                  height={90}
+                  unoptimized
+                />
+              </a>
+            </Col>
 
-          <Col xl="3" md="4" sm="9" xs="9">
-            <div className="d-flex" style={{ width: "auto" }}>
-              <label htmlFor="doc_search" className="visually-hidden">
-                Search:
-              </label>
-              <input
-                id="doc_search"
-                type="text"
-                name="search"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                onKeyDown={handleKey}
-                className="form-control"
-                placeholder="Document Site Search"
-                style={{
-                  borderTopLeftRadius: "20px",
-                  borderBottomLeftRadius: "20px",
-                  borderBottomRightRadius: "0",
-                  borderTopRightRadius: "0",
-                }}
-              />
-              <div
-                className="input-group-text"
-                onClick={handleClick}
-                style={{
-                  borderTopRightRadius: "20px",
-                  borderBottomRightRadius: "20px",
-                  borderBottomLeftRadius: "0",
-                  borderTopLeftRadius: "0",
-                  marginLeft: "-1px",
-                  cursor: "pointer",
-                }}
-              >
-                <i className="bi bi-search"></i>
+            <Col xl="3" md="4" sm="9" xs="9">
+              <div className="d-flex" style={{ width: "auto" }}>
+                <label htmlFor="doc_search" className="visually-hidden">
+                  Search:
+                </label>
+                <input
+                  id="doc_search"
+                  type="text"
+                  name="search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  onKeyDown={handleKey}
+                  className="form-control"
+                  placeholder="Document Site Search"
+                  style={{
+                    borderTopLeftRadius: "20px",
+                    borderBottomLeftRadius: "20px",
+                    borderBottomRightRadius: "0",
+                    borderTopRightRadius: "0",
+                  }}
+                />
+                <div
+                  className="input-group-text"
+                  onClick={handleClick}
+                  style={{
+                    borderTopRightRadius: "20px",
+                    borderBottomRightRadius: "20px",
+                    borderBottomLeftRadius: "0",
+                    borderTopLeftRadius: "0",
+                    marginLeft: "-1px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <i className="bi bi-search"></i>
+                </div>
               </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-
-      <AppNavbar routes={routes} />
-    </header>
+            </Col>
+          </Row>
+        </Container>
+        <AppNavbar routes={routes} />
+      </header>
+    </>
   );
 }
