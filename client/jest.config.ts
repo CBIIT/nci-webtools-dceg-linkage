5c224ab549ffc6d9ee476abaf9e690caf468172c
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-07-08 14:18:44 -0400

    fix module import map

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -100,7 +100,7 @@ const config: Config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
