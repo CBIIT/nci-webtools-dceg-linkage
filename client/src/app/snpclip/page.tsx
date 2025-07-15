ad3cefa2916601b1e377ca115e4520b70da7cf59
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2025-07-15 15:10:34 -0400

    Add pages to SNPChip

---
+++ client/src/app/snpclip/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import Alert from "react-bootstrap/Alert";
+import { Container, Row, Col } from "react-bootstrap";
+import Form from "./form";
+import CalculateLoading from "@/components/calculateLoading";
+import ToolBanner from "@/components/toolBanner";
+
+const Results = dynamic(() => import("./results"), {
+  ssr: false,
+});
+
+export default function SNPchip() {
+  const searchParams = useSearchParams();
+  const ref = searchParams.get("ref");
+
+  return (
+    <>
+      <ToolBanner
+        name="LDhap Tool"
+        href="/help/#LDhap"
+        description="Calculate population specific haplotype frequencies of all haplotypes observed for a list of query variants."
+      />
+      <Container fluid="md">
+        <Row className="border rounded bg-white my-3 p-3 shadow-sm">
+          <Col>
+            <Form />
+            <ErrorBoundary errorComponent={() => <Alert variant="warning">Error loading results</Alert>}>
+              <Suspense fallback={<CalculateLoading />}>{ref && <Results ref={ref} />}</Suspense>
+            </ErrorBoundary>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
