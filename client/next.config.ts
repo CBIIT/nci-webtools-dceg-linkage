d275161281ec7ec6371fd20ba058f7ca1a0d58ec
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-08-21 12:40:15 -0400

    update proxy rewrite

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/LDlinkRestWeb/:path*",
-        destination: `http://${process.env.NEXT_PUBLIC_API_BASE_URL || "localhost:80"}/LDlinkRestWeb/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:80"}/LDlinkRestWeb/:path*`,
       },
       {
         source: "/api/:path*",
-        destination: `http://${process.env.NEXT_PUBLIC_API_BASE_URL || "localhost:8080"}/LDlinkRest/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:8080"}/LDlinkRest/:path*`,
       },
     ];
   },
