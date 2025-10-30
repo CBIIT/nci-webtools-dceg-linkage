127382297571c76b1fd24267e62b4f0d71c4bd2b
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-10-30 15:05:49 -0400

    update usa banner position

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -33,17 +33,17 @@ export function Header({ routes = [] }: HeaderProps) {
     <>
       <header>
         <section className="usa-banner" aria-label="Official government website">
-          <header className="usa-banner__header">
-            <div className="usa-banner__inner">
-              <div className="container">
-                <div className="usa-banner__header-text">An official website of the United States government</div>
-              </div>
-            </div>
-          </header>
+          <Container className="my-0 mx-autousa-banner__header">
+            <Row className="usa-banner__inner mx-0 px-0">
+              <Col xl="9" md="8" sm="12" className="usa-banner__header-text px-0">
+                An official website of the United States government
+              </Col>
+            </Row>
+          </Container>
         </section>
         <Container className="my-0">
           <Row className="align-items-center">
- <Col xl="9" md="8" sm="12">
+            <Col xl="9" md="8" sm="12">
               <a
                 href="https://dceg.cancer.gov/"
                 target="_blank"
@@ -85,17 +85,39 @@ export function Header({ routes = [] }: HeaderProps) {
                       `}
                     </style>
                   </defs>
-                  <path className="gray" d="M94.7,240l-14.5-26.1H27.8c-3.1,0-5.6,2.5-5.6,5.6v41c0,3.1,2.5,5.6,5.6,5.6h52.4L94.7,240z" />
-                  <path className="red" d="M93.3,216.6c-1-1.7-2.9-2.7-4.8-2.7h-4.3L98.8,240l-14.7,26.1h4.3c2,0,3.8-1,4.8-2.7l13.1-23.4L93.3,216.6z" />
+                  <path
+                    className="gray"
+                    d="M94.7,240l-14.5-26.1H27.8c-3.1,0-5.6,2.5-5.6,5.6v41c0,3.1,2.5,5.6,5.6,5.6h52.4L94.7,240z"
+                  />
+                  <path
+                    className="red"
+                    d="M93.3,216.6c-1-1.7-2.9-2.7-4.8-2.7h-4.3L98.8,240l-14.7,26.1h4.3c2,0,3.8-1,4.8-2.7l13.1-23.4L93.3,216.6z"
+                  />
                   <rect className="white" x="53.2" y="228.4" width="3.9" height="23.2" />
-                  <polygon className="white" points="45.4,228.4 45.4,245.4 34.8,228.4 30.9,228.4 30.9,251.6 34.8,251.6 34.8,234.6 45.4,251.6 49.3,251.6 49.3,228.4" />
-                  <polygon className="white" points="75.4,228.4 75.4,238.1 64.8,238.1 64.8,228.4 60.9,228.4 60.9,251.6 64.8,251.6 64.8,241.9 75.4,241.9 75.4,251.6 79.3,251.6 79.3,228.4" />
-                  <text className="red" x="117.5" y="232.5" fontFamily="Montserrat" fontWeight="700" fontSize="15.47">NATIONAL CANCER INSTITUTE</text>
-                  <text className="gray" x="117.2" y="258.5" fontFamily="Montserrat" fontWeight="700" fontSize="16" fontStyle="italic">LDlink</text>
+                  <polygon
+                    className="white"
+                    points="45.4,228.4 45.4,245.4 34.8,228.4 30.9,228.4 30.9,251.6 34.8,251.6 34.8,234.6 45.4,251.6 49.3,251.6 49.3,228.4"
+                  />
+                  <polygon
+                    className="white"
+                    points="75.4,228.4 75.4,238.1 64.8,238.1 64.8,228.4 60.9,228.4 60.9,251.6 64.8,251.6 64.8,241.9 75.4,241.9 75.4,251.6 79.3,251.6 79.3,228.4"
+                  />
+                  <text className="red" x="117.5" y="232.5" fontFamily="Montserrat" fontWeight="700" fontSize="15.47">
+                    NATIONAL CANCER INSTITUTE
+                  </text>
+                  <text
+                    className="gray"
+                    x="117.2"
+                    y="258.5"
+                    fontFamily="Montserrat"
+                    fontWeight="700"
+                    fontSize="16"
+                    fontStyle="italic">
+                    LDlink
+                  </text>
                 </svg>
               </a>
             </Col>
-
 
             <Col xl="3" md="4" sm="9" xs="9">
               <div className="d-flex align-items-center justify-content-center" style={{ width: "auto" }}>
