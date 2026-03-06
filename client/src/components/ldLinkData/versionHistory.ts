9e0db315597492094992acc94d6c003f6249976f
Judy Yao <xiaozheng.yao@nih.gov>
2026-03-06 18:09:21 -0500

    update version to 7.0.1

--- client/src/components/ldLinkData/versionHistory.ts
+++ client/src/components/ldLinkData/versionHistory.ts
@@ -5,7 +5,7 @@ export type NewsEntry = {
 
 export const versionHistory: NewsEntry[] = [
     {
-    title: "LDlink 7.0.0 Release (MM/DD/YYYY)",
+    title: "LDlink 7.0.0 Release (03/06/2026)",
     items: [
       "Credit reference added to LDscore",
       "Removed filename restrictions for LDScore input files",
